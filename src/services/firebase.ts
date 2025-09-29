eab0bbde0cdaaf2707ac6bb85eba0e7b92359895
choijooonil123 <choijooonil123@gmail.com>
2025-09-30 06:50:33 +0900

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,12 +5,17 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "YOUR_MSG_ID",
-appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD69ziT9DIBol_WYc_gbUmNf1gUoz0ix7k",
+  authDomain: "bible-listen.firebaseapp.com",
+  projectId: "bible-listen",
+
+  // ✅ 보통은 {projectId}.appspot.com 형식이 맞습니다.
+  // 콘솔 > Storage > 버킷 이름을 확인해서 정확히 넣어주세요.
+  storageBucket: "bible-listen.appspot.com",
+
+  messagingSenderId: "22812607084",
+  appId: "1:22812607084:web:790e8be5a45bc374b04e49",
+  measurementId: "G-YJG6CVEDKX"
 };
 
 
